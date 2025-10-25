dc8d7de279a8686125777efbc8e3696fba23764a
1f10230039 <s1f102300391@iniad.org>
2025-10-26 00:35:01 +0900

    ヘッダーの作成とレイアウト調整

---
+++ src/components/common/Header/Header.jsx
@@ -0,0 +1,52 @@
+// ヘッダーコンポーネント
+"use client";
+
+import styled from "@emotion/styled";
+import Navigation from "../Navigation/Navigation";
+import Link from "next/link";
+
+// Emotion
+// ヘッダー全体を包むコンテナ
+const HeaderContainer = styled.header`
+  width: 100%;
+  padding: 16px 24px;
+  background-color: #ffffff;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  /**スマホ(767px以下)ではロゴを中央に表示する */
+  @media (max-width: 767px) {
+    justify-content: center;
+  }
+`;
+
+// ロゴ用のスタイル
+const Logo = styled(Link)`
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+  text-decoration: none;
+`;
+
+// PC用のナビゲーションを囲むラッパー
+const DesktopNavigation = styled.div`
+  display: block;
+
+  /* スマホ（767px以下）では非表示にする */
+  @media (max-width: 767px) {
+    display: none;
+  }
+`;
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <Logo href="/">ボランティアApp</Logo>
+      <DesktopNavigation>
+        <Navigation />
+      </DesktopNavigation>
+    </HeaderContainer>
+  );
+}
